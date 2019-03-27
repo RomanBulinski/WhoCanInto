c4c583cb947c90854a12c33642ecd864a7e799e9
Roman Bulinski <aboandrus@gmail.com>
2019-03-27 11:41:42 +0100

    Implemented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,19 @@ public class Main {
 
         List<Person> whiteList = Arrays.asList(
                 new Person( "Rom", "111-222-333", Arrays.asList( "python","C" )  ),
-                new Person( "Tom", "111-222-333", Arrays.asList( "Java","C" )  )
+                new Person( "Tom", "111-222-333", Arrays.asList( "Java","C" )  ),
+                new Person( "Waldek", "111-222-333", Arrays.asList( "C" )  ),
+                new Person( "Jarek", "111-222-333", Arrays.asList( "Java" )  ),
+                new Person( "Tytus", "222-444-777", Arrays.asList( "Python","Java","C" )  )
         );
 
+//        whiteList.stream()
+//                .filter(person -> person.getName().equals("Rom") )
+//                .map(person -> person.getName() + " -  " + person.getPhone())
+//                .forEach(System.out::println);
 
         whiteList.stream()
-                .filter(person -> person.getName().equals("Rom") )
+                .filter(person -> person.getSkills().contains("Java") )
                 .map(person -> person.getName() + " -  " + person.getPhone())
                 .forEach(System.out::println);
 
